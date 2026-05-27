33b6b731ba0a644903c36ca5ace9bda75dae0ce5
Gustavo Queiroz <gustavoq.mateusgithub@gmail.com>
2026-05-27 15:56:37 -0300

    fix: restaura usuario do localStorage, adiciona rota protegida e redireciona ao sair

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import RotaProtegida from './components/RotaProtegida'
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro'
 import Tarefas from './pages/Tarefas'
@@ -12,7 +13,7 @@ export default function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/tarefas" element={<Tarefas />} />
+          <Route path="/tarefas" element={<RotaProtegida><Tarefas /></RotaProtegida>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
