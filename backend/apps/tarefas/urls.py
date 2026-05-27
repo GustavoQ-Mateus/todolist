79178a240037983a61f144c126dc84a2a3c9115c
Gustavo Queiroz <gustavoq.mateusgithub@gmail.com>
2026-05-27 10:50:57 -0300

    feat: adiciona endpoint de editar e excluir tarefa

--- backend/apps/tarefas/urls.py
+++ backend/apps/tarefas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TarefaListCreateView
+from .views import TarefaListCreateView, TarefaDetailView
 
 urlpatterns = [
     path('tarefas/', TarefaListCreateView.as_view()),
+    path('tarefas/<int:pk>/', TarefaDetailView.as_view()),
 ]
--- backend/apps/tarefas/views.py
+++ backend/apps/tarefas/views.py
@@ -9,3 +9,11 @@ class TarefaListCreateView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return Tarefa.objects.filter(criado_por=self.request.user)
+
+
+class TarefaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TarefaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Tarefa.objects.filter(criado_por=self.request.user)
