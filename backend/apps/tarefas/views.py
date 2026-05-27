1e3661e8133d5c86b5b9be976dcee71581a996a9
Gustavo Queiroz <gustavoq.mateusgithub@gmail.com>
2026-05-27 10:56:46 -0300

    feat: adiciona endpoints de categorias

--- backend/apps/tarefas/views.py
+++ backend/apps/tarefas/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions
-from .models import Tarefa
-from .serializers import TarefaSerializer
+from .models import Tarefa, Categoria
+from .serializers import TarefaSerializer, CategoriaSerializer
 
 
 class TarefaListCreateView(generics.ListCreateAPIView):
@@ -17,3 +17,22 @@ class TarefaDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Tarefa.objects.filter(criado_por=self.request.user)
+
+
+class CategoriaListCreateView(generics.ListCreateAPIView):
+    serializer_class = CategoriaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Categoria.objects.filter(criado_por=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(criado_por=self.request.user)
+
+
+class CategoriaDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CategoriaSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Categoria.objects.filter(criado_por=self.request.user)
