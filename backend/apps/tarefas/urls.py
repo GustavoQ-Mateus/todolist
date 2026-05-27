8329f518e662bbe1c87bb38e1755f69a1d83618f
Gustavo Queiroz <gustavoq.mateusgithub@gmail.com>
2026-05-27 10:56:46 -0300

    feat: adiciona endpoints de categorias

--- backend/apps/tarefas/urls.py
+++ backend/apps/tarefas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TarefaListCreateView, TarefaDetailView
+from .views import TarefaListCreateView, TarefaDetailView, CategoriaListCreateView, CategoriaDetailView
 
 urlpatterns = [
     path('tarefas/', TarefaListCreateView.as_view()),
     path('tarefas/<int:pk>/', TarefaDetailView.as_view()),
+    path('categorias/', CategoriaListCreateView.as_view()),
+    path('categorias/<int:pk>/', CategoriaDetailView.as_view()),
 ]
