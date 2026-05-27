105c5e491d33b42980ce71060e0cb68a946570f2
Gustavo Queiroz <gustavoq.mateusgithub@gmail.com>
2026-05-26 23:49:44 -0300

    feat: adiciona Dockerfile e docker-compose com PostgreSQL

---
+++ backend/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Timeout banco de dados"
+python -c "
+import socket, time
+while True:
+    try:
+        s = socket.create_connection(('db', 5432), timeout=1)
+        s.close()
+        break
+    except (socket.error, ConnectionRefusedError):
+        time.sleep(1)
+"
+echo "Banco On."
+python manage.py migrate
+python manage.py collectstatic --noinput
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
