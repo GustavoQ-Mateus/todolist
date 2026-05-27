8f9a177d0ffae82cc8bff9123b3fcd9c6fde8826
Gustavo Queiroz <gustavoq.mateusgithub@gmail.com>
2026-05-27 15:56:33 -0300

    feat: adiciona endpoint /usuarios/me/

--- backend/apps/usuarios/views.py
+++ backend/apps/usuarios/views.py
@@ -1,7 +1,16 @@
 from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializers import CadastroSerializer
 
 
 class CadastroView(generics.CreateAPIView):
     serializer_class = CadastroSerializer
     permission_classes = (permissions.AllowAny,)
+
+
+class MeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        return Response({'username': request.user.username, 'email': request.user.email})
